f14644afb97804af3a30726320d53e86ec04fb9f
Robert Larsen <robert.larsen@nav.no>
2019-10-10 11:03:18 +0200

    [PB-117] Veldig midlertidig løsning for at vi kan få typen til eventet sendt til frontend. Foreløpig støttes bare èn type, Informasjon.

--- src/main/kotlin/no/nav/personbruker/dittnav/api/melding/Melding.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/melding/Melding.kt
@@ -1,7 +1,10 @@
 package no.nav.personbruker.dittnav.api.melding
 
+import no.nav.personbruker.dittnav.api.event.EventType
+
 data class Melding(
     val id: String,
+    val type: EventType,
     val link: String,
     val sikkerhetsnivaa: Int,
     val sistOppdatert: String,
