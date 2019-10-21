481a3f4142a32389f0350cbe6055018caea77b23
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2019-10-21 17:00:10 +0200

    Lagt til støtte for å hente ut OIDC-token-et enten fra authorization-header-en eller fra en cookie.
    PB-239. DittNAV-api - Ta i bruk NAVs sikkerhetsbibliotek for OIDC

--- src/main/kotlin/no/nav/personbruker/dittnav/api/legacy/LegacyConsumer.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/legacy/LegacyConsumer.kt
@@ -7,17 +7,16 @@ import io.ktor.client.request.url
 import io.ktor.client.response.HttpResponse
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import io.ktor.http.auth.HttpAuthHeader
 import no.nav.personbruker.dittnav.api.config.Environment
 
 class LegacyConsumer(private val httpClient: HttpClient, private val environment: Environment) {
 
-    suspend fun getLegacyContent(url: String, authHeader: HttpAuthHeader?): HttpResponse {
+    suspend fun getLegacyContent(url: String, token: String): HttpResponse {
         return httpClient.use { client ->
             client.request {
                 url(environment.dittNAVLegacyURL + url)
                 method = HttpMethod.Get
-                header(HttpHeaders.Authorization, authHeader)
+                header(HttpHeaders.Authorization, "Bearer $token")
             }
         }
     }
