f16dcb49766a6dba2a37e97ebef5ba57d9a3255e
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2020-02-05 10:00:12 +0100

    Mock-er ut JwtToken så ikke get() returnerer null i testene
    feature/PB-334-henter-innloggetbruker-fra-context

--- src/test/kotlin/no/nav/personbruker/dittnav/api/common/InnloggetBrukerObjectMother.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/common/InnloggetBrukerObjectMother.kt
@@ -3,6 +3,7 @@ package no.nav.personbruker.dittnav.api.common
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.security.token.support.core.context.TokenValidationContext
+import no.nav.security.token.support.core.jwt.JwtToken
 import no.nav.security.token.support.ktor.OIDCValidationContextPrincipal
 
 fun createInnloggetBruker(): InnloggetBruker {
@@ -13,5 +14,10 @@ fun createInnloggetBruker(): InnloggetBruker {
 
 fun createDummyTokenSupport(): OIDCValidationContextPrincipal {
     val tokenValidationContext = mockk<TokenValidationContext>()
-    return OIDCValidationContextPrincipal(tokenValidationContext)
+    val oidcValidationContextPrincipal = OIDCValidationContextPrincipal(tokenValidationContext)
+    val dummyJwtToken = mockk<JwtToken>()
+
+    every { oidcValidationContextPrincipal.context.firstValidToken.get() } returns dummyJwtToken
+
+    return oidcValidationContextPrincipal
 }
