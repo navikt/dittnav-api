c0ab21890a10999cc47aabf88431ac11682a7aa3
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2020-02-06 09:20:35 +0100

    Mock-er ut firstValidToken så ikke testene feiler
    feature/PB-334-henter-innloggetbruker-fra-context

--- src/test/kotlin/no/nav/personbruker/dittnav/api/common/InnloggetBrukerObjectMother.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/common/InnloggetBrukerObjectMother.kt
@@ -5,6 +5,7 @@ import io.mockk.mockk
 import no.nav.security.token.support.core.context.TokenValidationContext
 import no.nav.security.token.support.core.jwt.JwtToken
 import no.nav.security.token.support.ktor.OIDCValidationContextPrincipal
+import java.util.*
 
 object InnloggetBrukerObjectMother {
 
@@ -18,6 +19,8 @@ object InnloggetBrukerObjectMother {
         val tokenValidationContext = mockk<TokenValidationContext>()
         val oidcValidationContextPrincipal = OIDCValidationContextPrincipal(tokenValidationContext)
         val dummyJwtToken = mockk<JwtToken>()
+        val dummyToken: Optional<JwtToken> = mockk<Optional<JwtToken>>()
+        every { oidcValidationContextPrincipal.context.firstValidToken } returns dummyToken
         every { oidcValidationContextPrincipal.context.firstValidToken.get() } returns dummyJwtToken
         return oidcValidationContextPrincipal
     }
