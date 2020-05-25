c3e81b65e0c97fcd4bf7b69035d54a977e017998
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-05-25 17:10:17 +0200

    Lagt til endepunkter for å hente antall brukernotifikasjoner fra event-handler. Det støttes å hente antallet aktive, inaktive og totalantallet brukernotifikasjoner.

---
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/brukernotifikasjon/BrukernotifikasjonConsumer.kt
@@ -0,0 +1,29 @@
+package no.nav.personbruker.dittnav.api.brukernotifikasjon
+
+import io.ktor.client.HttpClient
+import no.nav.personbruker.dittnav.api.common.InnloggetBruker
+import no.nav.personbruker.dittnav.api.config.get
+import java.net.URL
+
+class BrukernotifikasjonConsumer(
+        private val client: HttpClient,
+        private val eventHandlerBaseURL: URL,
+        private val pathToEndpoint: URL = URL("$eventHandlerBaseURL/count/brukernotifikasjoner")
+) {
+
+    suspend fun countInactive(innloggetBruker: InnloggetBruker): Int {
+        val completePathToEndpoint = URL("$pathToEndpoint/inactive")
+        return client.get(completePathToEndpoint, innloggetBruker)
+    }
+
+    suspend fun countActive(innloggetBruker: InnloggetBruker): Int {
+        val completePathToEndpoint = URL("$pathToEndpoint/active")
+        return client.get(completePathToEndpoint, innloggetBruker)
+    }
+
+    suspend fun count(innloggetBruker: InnloggetBruker): Int {
+        val completePathToEndpoint = URL("$pathToEndpoint")
+        return client.get(completePathToEndpoint, innloggetBruker)
+    }
+
+}
---
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/brukernotifikasjon/BrukernotifikasjonService.kt
@@ -0,0 +1,35 @@
+package no.nav.personbruker.dittnav.api.brukernotifikasjon
+
+import no.nav.personbruker.dittnav.api.common.ConsumeEventException
+import no.nav.personbruker.dittnav.api.common.InnloggetBruker
+
+class BrukernotifikasjonService(private val brukernotifikasjonConsumer: BrukernotifikasjonConsumer) {
+
+    suspend fun totalNumberOfEvents(innloggetBruker: InnloggetBruker): Int {
+        return try {
+            brukernotifikasjonConsumer.count(innloggetBruker)
+
+        } catch (exception: Exception) {
+            throw ConsumeEventException("Klarte ikke å finne ut om brukeren har brukernotifikasjoner", exception)
+        }
+    }
+
+    suspend fun numberOfInactive(innloggetBruker: InnloggetBruker): Int {
+        return try {
+            brukernotifikasjonConsumer.countInactive(innloggetBruker)
+
+        } catch (exception: Exception) {
+            throw ConsumeEventException("Klarte ikke å finne ut om brukeren har inaktive brukernotifikasjoner", exception)
+        }
+    }
+
+    suspend fun numberOfActive(innloggetBruker: InnloggetBruker): Int {
+        return try {
+            brukernotifikasjonConsumer.countActive(innloggetBruker)
+
+        } catch (exception: Exception) {
+            throw ConsumeEventException("Klarte ikke å finne ut om brukeren har aktive brukernotifikasjoner", exception)
+        }
+    }
+
+}
