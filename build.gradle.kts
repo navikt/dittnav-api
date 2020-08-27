56a28a111d61201907e0ece6750ad4968080ae2d
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2020-08-27 17:16:33 +0200

    Flyttet miljøvariablene for å kjøre appen lokalt, mot docker-compose, ut i dittnav-dependencies slik at disse kan gjenbrukes i alle appene våre.
    PB-458. Forenkle oppdatering av avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,14 +87,11 @@ tasks {
     }
 
     register("runServer", JavaExec::class) {
-        environment("LEGACY_API_URL", "http://localhost:8090/person/dittnav-legacy-api")
-        environment("EVENT_HANDLER_URL", "http://localhost:8092")
-        environment("CORS_ALLOWED_ORIGINS", "localhost:9002")
-
-        environment("OIDC_ISSUER", "http://localhost:9000")
-        environment("OIDC_DISCOVERY_URL", "http://localhost:9000/.well-known/openid-configuration")
-        environment("OIDC_ACCEPTED_AUDIENCE", "stubOidcClient")
-        environment("OIDC_CLAIM_CONTAINING_THE_IDENTITY", "pid")
+        println("Setting default environment variables for running with DittNAV docker-compose")
+        DockerComposeDefaults.environomentVariables.forEach { (name, value) ->
+            println("Setting the environment variable $name")
+            environment(name, value)
+        }
 
         main = application.mainClassName
         classpath = sourceSets["main"].runtimeClasspath
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2020.08.27-12.41-ec847a68d5e0"
+val dittNavDependenciesVersion = "2020.08.27-17.07-d653e3deee18"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
