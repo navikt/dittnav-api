7bda5f79eb85509e6b6c3796b664701112250fa3
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-10-05 12:52:30 +0200

    Endret logging fra warn -> info

--- src/main/kotlin/no/nav/personbruker/dittnav/api/logging/logTokenExpiry.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/logging/logTokenExpiry.kt
@@ -8,7 +8,7 @@ fun logTokenExpiry(log: Logger, call: ApplicationCall) {
     val headers: ResponseHeaders = call.response.headers
 
     if (headers.contains("x-token-expires-soon")) {
-        log.warn("Et kall fra dittnav er gjort med et token som har to minutter igjen.")
+        log.info("Et kall fra dittnav er gjort med et token som har to minutter igjen.")
     }
 }
 
