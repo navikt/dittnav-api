05adb8fb5e3d5fdec188111e93edd8d56499aeaf
Christopher Santana <christopher.santana@nav.no>
2020-12-01 11:33:32 +0100

    Fjernet debug logging.

--- src/main/kotlin/no/nav/personbruker/dittnav/api/beskjed/BeskjedVarselSwitcher.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/beskjed/BeskjedVarselSwitcher.kt
@@ -1,7 +1,6 @@
 package no.nav.personbruker.dittnav.api.beskjed
 
 import no.nav.personbruker.dittnav.api.unleash.UnleashService
-import no.nav.personbruker.dittnav.common.logging.util.logger
 import no.nav.personbruker.dittnav.common.security.AuthenticatedUser
 
 class BeskjedVarselSwitcher(
@@ -12,22 +11,18 @@ class BeskjedVarselSwitcher(
 
     suspend fun getActiveEvents(user: AuthenticatedUser): BeskjedResult {
         return if (unleashService.mergeBeskjedVarselEnabled(user)) {
-            logger.info("Viser aktive beskjeder og varsler.")
             beskjedMedVarselService.getActiveEvents(user)
 
         } else {
-            logger.info("Viser kun aktive beskjeder.")
             beskjedService.getActiveBeskjedEvents(user)
         }
     }
 
     suspend fun getInactiveEvents(user: AuthenticatedUser): BeskjedResult {
         return if (unleashService.mergeBeskjedVarselEnabled(user)) {
-            logger.info("Viser inaktive beskjeder og varsler.")
             beskjedMedVarselService.getInactiveEvents(user)
 
         } else {
-            logger.info("Viser kun inaktive beskjeder.")
             beskjedService.getInactiveBeskjedEvents(user)
         }
     }
