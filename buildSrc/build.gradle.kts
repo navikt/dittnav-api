91764e9701d46a45bf013934c6c5e625fd452844
Robert Larsen <robert.larsen@nav.no>
2021-03-05 17:10:38 +0100

    Revert "Ny versjon av dittnav-dependencies, med nyeste versjoner av avhengighetene"
    This reverts commit cbc99a33f40f942b93b0f581e7e61916dc7de266.
    
    # Conflicts:
    #	buildSrc/build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.03.05-12.55-7ea26b8f8982"
+val dittNavDependenciesVersion = "2021.01.28-13.10-57381c88aaab"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
