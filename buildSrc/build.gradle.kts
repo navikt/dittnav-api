45da141f841f3ac83e9c27b78fde390e45be5aa8
Robert Larsen <robert.larsen@nav.no>
2021-03-05 13:00:23 +0100

    Ny versjon av dittnav-dependencies, der bare Ktor er oppdatert.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2021.03.05-07.47-f7531b5f7606"
+val dittNavDependenciesVersion = "2021.03.05-12.55-7ea26b8f8982"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
