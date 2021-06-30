053399ec9b11f7bae377c95f0ddd16a86bcb19bf
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-06-30 08:58:44 +0200

    Trukket inn en reell respons fra DigiSos i deserialiserings-testen.
    PB-772. Sy sammen digisos med brukernotifikasjoner

--- src/test/kotlin/no/nav/personbruker/dittnav/api/digisos/PaabegynteTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/digisos/PaabegynteTest.kt
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Test
 internal class PaabegynteTest {
 
     private val respons = """
-        {
-            "eventTidspunkt": "2021-06-17T09:32:35.416897",
-            "eventId": "123456",
-            "grupperingsId": "enId",
-            "tekst": "Søknad om økonomisk sosialhjelp",
-            "link": "https://dummy/enId",
-            "sikkerhetsnivaa": 3,
-            "sisteOppdatert": "2021-07-01T09:32:35.416856",
-            "aktiv": false
-        }""".trimIndent()
+    {
+        "eventTidspunkt": "2021-06-17T09:32:35.416897",
+        "eventId": "123456",
+        "grupperingsId": "enId",
+        "tekst": "Søknad om økonomisk sosialhjelp",
+        "link": "https://dummy/enId",
+        "sikkerhetsnivaa": 3,
+        "sistOppdatert": "2021-07-01T09:32:35.416856",
+        "aktiv": false
+    }""".trimIndent()
 
     @Test
     fun `Skal kunne deserialisere en respons fra DigiSos`() {
