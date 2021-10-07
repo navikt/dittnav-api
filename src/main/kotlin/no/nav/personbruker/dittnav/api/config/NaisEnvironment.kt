85373ed358a762b44c83fb13bcf5230476d8ff22
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-10-07 15:34:28 +0200

    La til cluster i unleash sin kontekst.
    PB-854. Dittnav-api: sett opp unleash for mine saker

--- src/main/kotlin/no/nav/personbruker/dittnav/api/config/NaisEnvironment.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/config/NaisEnvironment.kt
@@ -2,9 +2,11 @@ package no.nav.personbruker.dittnav.api.config
 
 object NaisEnvironment {
 
+    fun currentClusterName() : String? = System.getenv("NAIS_CLUSTER_NAME")
+
     fun isRunningInProd(): Boolean {
-        val currentEnvironment = System.getenv("NAIS_CLUSTER_NAME")
-        return currentEnvironment == "prod-sbs" || currentEnvironment == "prod-gcp"
+        val clusterName = currentClusterName()
+        return clusterName == "prod-sbs" || clusterName == "prod-gcp"
     }
 
     fun isRunningInDev(): Boolean {
