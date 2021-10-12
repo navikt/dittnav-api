e196bd0ddb5cdfe247bc142b39ef3ea9961c200e
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-10-12 14:15:53 +0200

    Innfører ny ekstern DTO som kan brukes for å deserialisere det nye formatet fra Mine Saker.
    PB-889. Henting av siste skal alltid inneholde info om dagpenger

---
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/saker/ekstern/SisteSakstemaer.kt
@@ -0,0 +1,16 @@
+@file:UseSerializers(ZonedDateTimeSerializer::class, URLSerializer::class)
+
+package no.nav.personbruker.dittnav.api.saker.ekstern
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import no.nav.personbruker.dittnav.api.common.serializer.URLSerializer
+import no.nav.personbruker.dittnav.api.common.serializer.ZonedDateTimeSerializer
+import no.nav.personbruker.dittnav.api.saker.Sakstema
+import java.time.ZonedDateTime
+
+@Serializable
+data class SisteSakstemaer(
+    val sistEndrede: List<Sakstema>,
+    val dagpengerSistEndret: ZonedDateTime
+)
---
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/saker/ekstern/SisteSakstemaerTest.kt
@@ -0,0 +1,36 @@
+package no.nav.personbruker.dittnav.api.saker.ekstern
+
+import kotlinx.serialization.decodeFromString
+import no.nav.personbruker.dittnav.api.config.json
+import org.amshove.kluent.shouldNotBeNull
+import org.junit.jupiter.api.Test
+
+internal class SisteSakstemaerTest {
+
+    val mineSakerRespons = """
+        {
+          "sistEndrede" : [ {
+            "navn" : "Økonomisk sosialhjelp",
+            "kode" : "KOM",
+            "sistEndret" : "2021-10-07T09:01:16.822189+02:00",
+            "detaljvisningUrl" : "https://dummy/KOM"
+          }, {
+            "navn" : "Pensjon",
+            "kode" : "PEN",
+            "sistEndret" : "2021-08-13T09:01:16.826011+02:00",
+            "detaljvisningUrl" : "https://dummy/PEN"
+          } ],
+          "dagpengerSistEndret" : "2021-07-04T09:01:16.826137+02:00"
+        }
+    """.trimIndent()
+
+    @Test
+    fun `Skal kunne deserialisere ekstern respons`() {
+        val om = json()
+
+        val deserialized = om.decodeFromString<SisteSakstemaer>(mineSakerRespons)
+
+        deserialized.shouldNotBeNull()
+    }
+
+}
