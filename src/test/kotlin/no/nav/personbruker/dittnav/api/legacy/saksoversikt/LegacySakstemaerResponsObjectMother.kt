6c86b1c8748f3013a806a13761cbffa20c17d6f9
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2021-11-01 09:37:06 +0100

    La til tester som verifiserer de forskjellige kombinasjonene av å ha eller ikke ha hatt dagpenger. Verifiserer logikken for resultater som hentes fra gamle Saksoversikt.
    PB-889. Henting av siste skal alltid inneholde info om dagpenger

--- src/test/kotlin/no/nav/personbruker/dittnav/api/legacy/saksoversikt/LegacySakstemaerResponsObjectMother.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/legacy/saksoversikt/LegacySakstemaerResponsObjectMother.kt
@@ -2,9 +2,34 @@ package no.nav.personbruker.dittnav.api.legacy.saksoversikt
 
 object LegacySakstemaerResponsObjectMother {
 
-    fun giveMeLegacyResponseWithDagpenger(): LegacySakstemaerRespons {
+    fun giveMeLegacyResponseWithDagpengerSomEnAvDeToSiste(): LegacySakstemaerRespons {
         val sakstemaer = listOf(
             LegacySakstemaObjectMother.giveMeSakstemaDagpenger(),
+            LegacySakstemaObjectMother.giveMeSakstemaOppfolging(),
+            LegacySakstemaObjectMother.giveMeSakstemaBil(),
+            LegacySakstemaObjectMother.giveMeSakstemaSosialhjelp()
+        )
+        return LegacySakstemaerRespons(
+            sakstemaer.size,
+            sakstemaer
+        )
+    }
+
+    fun giveMeLegacyResponseWithDagpenger(): LegacySakstemaerRespons {
+        val sakstemaer = listOf(
+            LegacySakstemaObjectMother.giveMeSakstemaOppfolging(),
+            LegacySakstemaObjectMother.giveMeSakstemaBil(),
+            LegacySakstemaObjectMother.giveMeSakstemaSosialhjelp(),
+            LegacySakstemaObjectMother.giveMeSakstemaDagpenger()
+        )
+        return LegacySakstemaerRespons(
+            sakstemaer.size,
+            sakstemaer
+        )
+    }
+
+    fun giveMeLegacyResponseWithoutDagpenger(): LegacySakstemaerRespons {
+        val sakstemaer = listOf(
             LegacySakstemaObjectMother.giveMeSakstemaOppfolging(),
             LegacySakstemaObjectMother.giveMeSakstemaBil(),
             LegacySakstemaObjectMother.giveMeSakstemaSosialhjelp()
--- src/test/kotlin/no/nav/personbruker/dittnav/api/legacy/saksoversikt/LegacyTransformerTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/legacy/saksoversikt/LegacyTransformerTest.kt
@@ -2,13 +2,27 @@ package no.nav.personbruker.dittnav.api.legacy.saksoversikt
 
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should contain`
+import org.amshove.kluent.shouldBeNull
 import org.amshove.kluent.shouldNotBeNull
 import org.junit.jupiter.api.Test
 
 internal class LegacyTransformerTest {
 
     @Test
-    fun `Skal kunne transformere fra LegacySakstemaerRespons til intern modell`() {
+    fun `Skal stotte at dagpenger er en av de to siste sakstemaene`() {
+        val externals = LegacySakstemaerResponsObjectMother.giveMeLegacyResponseWithDagpengerSomEnAvDeToSiste()
+
+        val internal = externals.toInternal()
+
+        internal.sakstemaer.size `should be equal to` 2
+        internal.dagpengerSistEndret.shouldNotBeNull()
+        val dagpengerSomEnAvDeToSiste = internal.sakstemaer[0]
+        dagpengerSomEnAvDeToSiste.kode `should be equal to` "DAG"
+        internal.dagpengerSistEndret `should be equal to` dagpengerSomEnAvDeToSiste.sistEndret
+    }
+
+    @Test
+    fun `Skal stotte at dagpenger IKKE er en av de to siste, og plukke ut sist endret datoen for den gamle dagpenge saken`() {
         val externals = LegacySakstemaerResponsObjectMother.giveMeLegacyResponseWithDagpenger()
 
         val internal = externals.toInternal()
@@ -18,7 +32,17 @@ internal class LegacyTransformerTest {
     }
 
     @Test
-    fun `Skal kunne transformere fra LegacySakstema til intern modell`() {
+    fun `Skal stotte at en bruker aldri har hatt dagpenger noen gang, da skal de ikke returneres dato for dagpenger`() {
+        val externals = LegacySakstemaerResponsObjectMother.giveMeLegacyResponseWithoutDagpenger()
+
+        val internal = externals.toInternal()
+
+        internal.sakstemaer.size `should be equal to` 2
+        internal.dagpengerSistEndret.shouldBeNull()
+    }
+
+    @Test
+    fun `Skal kunne transformere fra et konkret eksternt sakstema til intern modell`() {
         val external = LegacySakstemaObjectMother.giveMeSakstemaDagpenger()
 
         val internal = external.toInternal()
