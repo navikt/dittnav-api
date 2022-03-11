434c5221e33ad984ea786cca1c72372fdcb3e8c8
Christopher Santana <christopher.santana@nav.no>
2022-03-11 09:52:43 +0100

    Forsøk på å håndtere tomt objekt fra meldekort-api.

--- src/main/kotlin/no/nav/personbruker/dittnav/api/meldekort/external/MeldekortstatusExternal.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/meldekort/external/MeldekortstatusExternal.kt
@@ -6,9 +6,9 @@ import java.time.LocalDate
 
 @Serializable
 data class MeldekortstatusExternal(
-    val meldekort: Int,
-    val etterregistrerteMeldekort: Int,
-    val antallGjenstaaendeFeriedager: Int,
-    val nesteMeldekort: MeldekortExternal?,
-    @Serializable(with = MeldekortLocalDateSerializer::class) val nesteInnsendingAvMeldekort: LocalDate?,
+    val meldekort: Int = 0,
+    val etterregistrerteMeldekort: Int = 0,
+    val antallGjenstaaendeFeriedager: Int = 0,
+    val nesteMeldekort: MeldekortExternal? = null,
+    @Serializable(with = MeldekortLocalDateSerializer::class) val nesteInnsendingAvMeldekort: LocalDate? = null,
 )
