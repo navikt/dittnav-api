1c2ea0b394f0d7b26cb2f1943742ed62b8e6b46f
Robert Larsen <robert.larsen@nav.no>
2022-06-29 13:37:15 +0200

    Fjerner bruk av Unleash ifm merging av beskjeder/varsler fra varselinnboks.
    Denne funksjonaliteten blir ikke nødvendig, siden varselinnboks skal skrus av.

--- src/test/kotlin/no/nav/personbruker/dittnav/api/beskjed/BeskjedMergerServiceTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/api/beskjed/BeskjedMergerServiceTest.kt
@@ -1,11 +1,7 @@
 package no.nav.personbruker.dittnav.api.beskjed
 
 import io.kotest.matchers.shouldBe
-import io.mockk.clearMocks
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.confirmVerified
-import io.mockk.mockk
+import io.mockk.*
 import kotlinx.coroutines.runBlocking
 import no.finn.unleash.FakeUnleash
 import no.nav.personbruker.dittnav.api.common.AuthenticatedUserObjectMother
@@ -67,29 +63,6 @@ internal class BeskjedMergerServiceTest {
     }
 
     @Test
-    fun `Hent aktive fra Varselinnboks hvis aktivert`() {
-        val fakeUnleash = FakeUnleash().apply {
-            enable(UnleashService.varselinnboksToggleName)
-        }
-        val unleashService = UnleashService(fakeUnleash)
-
-        val beskjedMerger = BeskjedMergerService(beskjedService, digiSosService, unleashService)
-
-        val result = runBlocking {
-            beskjedMerger.getActiveEvents(innloggetBruker)
-        }
-
-        coVerify(exactly = 1) { beskjedService.getActiveBeskjedEvents(any()) }
-        coVerify(exactly = 0) { digiSosService.getPaabegynteActive(any()) }
-
-        confirmVerified(beskjedService)
-        confirmVerified(digiSosService)
-
-        result.successFullSources().size shouldBe 1
-        result.successFullSources() shouldBe listOf(KildeType.EVENTHANDLER)
-    }
-
-    @Test
     fun `Hent påbegynte søknader fra DigiSos hvis aktivert`() {
         val fakeUnleash = FakeUnleash().apply {
             enable(UnleashService.digisosPaabegynteToggleName)
@@ -115,7 +88,6 @@ internal class BeskjedMergerServiceTest {
     @Test
     fun `Hent aktive fra alle kilder hvis aktivert`() {
         val fakeUnleash = FakeUnleash().apply {
-            enable(UnleashService.varselinnboksToggleName)
             enable(UnleashService.digisosPaabegynteToggleName)
         }
         val unleashService = UnleashService(fakeUnleash)
@@ -139,29 +111,6 @@ internal class BeskjedMergerServiceTest {
     @Test
     fun `Hent inaktive alltid fra event-handler`() {
         val fakeUnleash = FakeUnleash()
-        val unleashService = UnleashService(fakeUnleash)
-
-        val beskjedMerger = BeskjedMergerService(beskjedService, digiSosService, unleashService)
-
-        val result = runBlocking {
-            beskjedMerger.getInactiveEvents(innloggetBruker)
-        }
-
-        coVerify(exactly = 1) { beskjedService.getInactiveBeskjedEvents(any()) }
-        coVerify(exactly = 0) { digiSosService.getPaabegynteInactive(any()) }
-
-        confirmVerified(beskjedService)
-        confirmVerified(digiSosService)
-
-        result.successFullSources().size shouldBe 1
-        result.successFullSources() shouldBe listOf(KildeType.EVENTHANDLER)
-    }
-
-    @Test
-    fun `Hent inaktive fra Varselinnboks hvis aktivert`() {
-        val fakeUnleash = FakeUnleash().apply {
-            enable(UnleashService.varselinnboksToggleName)
-        }
         val unleashService = UnleashService(fakeUnleash)
 
         val beskjedMerger = BeskjedMergerService(beskjedService, digiSosService, unleashService)
@@ -206,7 +155,6 @@ internal class BeskjedMergerServiceTest {
     @Test
     fun `Hent inaktive fra alle kilder hvis aktivert`() {
         val fakeUnleash = FakeUnleash().apply {
-            enable(UnleashService.varselinnboksToggleName)
             enable(UnleashService.digisosPaabegynteToggleName)
         }
         val unleashService = UnleashService(fakeUnleash)
@@ -225,5 +173,4 @@ internal class BeskjedMergerServiceTest {
         result.successFullSources().size shouldBe 2
         result.successFullSources() shouldBe listOf(KildeType.EVENTHANDLER, KildeType.DIGISOS)
     }
-
 }
