34cfbe3bda4261f8c7e14cd1286f831e35b3a58d
Christopher Santana <christopher.santana@nav.no>
2022-09-09 12:48:15 +0200

    Håndter begge datetime-format.

--- src/main/kotlin/no/nav/personbruker/dittnav/api/meldekort/MeldekortLocalDateSerializer.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/meldekort/MeldekortLocalDateSerializer.kt
@@ -7,12 +7,17 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 class MeldekortLocalDateSerializer: KSerializer<LocalDate> {
-    private val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val serializedDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
     override fun deserialize(decoder: Decoder): LocalDate {
-        return LocalDate.parse(decoder.decodeString(), dateFormat)
+        val timeString = decoder.decodeString()
+
+        return parseDateTimeFormat(timeString)
+            ?: parseDateFormat(timeString)
+            ?: throw RuntimeException("Forsøkte å parse dato på uventet format: $timeString")
     }
 
     override val descriptor = PrimitiveSerialDescriptor(
@@ -21,9 +26,25 @@ class MeldekortLocalDateSerializer: KSerializer<LocalDate> {
     )
 
     override fun serialize(encoder: Encoder, value: LocalDate) {
-        val dateString = value.format(dateFormat)
+        val dateString = value.format(serializedDateFormat)
 
         encoder.encodeString(dateString)
+    }
+
+    private fun parseDateTimeFormat(timeString: String): LocalDate? {
+        return try {
+            LocalDate.parse(timeString, DateTimeFormatter.ISO_DATE_TIME)
+        } catch (e: DateTimeParseException) {
+            null
+        }
+    }
+
+    private fun parseDateFormat(timeString: String): LocalDate? {
+        return try {
+            LocalDate.parse(timeString, DateTimeFormatter.ISO_DATE)
+        } catch (e: DateTimeParseException) {
+            null
+        }
     }
 
 }
