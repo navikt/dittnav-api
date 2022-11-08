ebb39caac1b26a7366a1a29177571acdee4427fb
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-11-08 10:41:45 +0100

    Debug: prøver å catche throwable

--- src/main/kotlin/no/nav/personbruker/dittnav/api/personalia/PersonaliaConsumer.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/personalia/PersonaliaConsumer.kt
@@ -25,6 +25,8 @@ class PersonaliaConsumer (
     }
 
     private suspend fun sendQuery(ident: String, token: String): GraphQLClientResponse<HentNavn.Result> {
+        log.info("Henter navn fra url: $pdlUrl")
+
         try {
             val hentNavnQuery = HentNavn(HentNavn.Variables(ident = ident))
             return client.execute(hentNavnQuery) {
--- src/main/kotlin/no/nav/personbruker/dittnav/api/personalia/personaliaApi.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/api/personalia/personaliaApi.kt
@@ -20,9 +20,10 @@ fun Route.personalia(
             val result = service.hentNavn(authenticatedUser)
             call.respond(HttpStatusCode.OK, result)
 
-        } catch (exception: Exception) {
-            log.info { "Klarte ikke å hente navn fra pdl" }
-            respondWithError(call, log, exception)
+        } catch (throwable: Throwable) {
+            log.warn("Klarte ikke å hente navn fra pdl")
+            log.error("Catchet throwable $throwable med stacktrace: ${throwable.stackTraceToString()}")
+            // respondWithError(call, log, exception)
         }
 
     }
