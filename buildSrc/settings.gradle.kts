104c6d81be529fcc871e6b0dd1760fde7b55ac08
Christopher Santana <christopher.santana@nav.no>
2023-02-01 13:23:23 +0100

    Tilpass dependencies.

--- buildSrc/settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-/*
-Fixes annoying gradle idea plugin bug. The idea gradle plugin expects unique root project name for buildSrc in gradle composite builds
-https://github.com/gradle/gradle/issues/8920
- */
-
-rootProject.name = rootProject.projectDir.parentFile.name + "-buildSrc"
